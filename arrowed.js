6d5f94bca4a4010703553f613242487159b7e6da
Jesse Chen <jesse.z.chen@gmail.com>
2024-09-12 14:32:31 -0400

    Draw a fixed set of arrows

--- arrowed.js
+++ arrowed.js
@@ -1,1 +1,42 @@
-console.log('It works!');
+const ARROW_HEIGHT = 100;
+const ARROW_WIDTH = 100;
+
+
+let stream = "ULRDULRDURLDURLD".split("");
+
+const numBeats = stream.length;
+const canvas = document.querySelector("canvas");
+canvas.setAttribute("height", ARROW_HEIGHT / 2 * (numBeats + 1));
+canvas.setAttribute("width", ARROW_WIDTH * 4);
+const context = canvas.getContext("2d");
+
+let y = 0;
+for (let beat of stream) {
+    let positions = arrowPositions(beat);
+    for (let position of positions) {
+        drawArrow(position, y);
+    }
+    y += ARROW_HEIGHT / 2;
+}
+
+function drawArrow(position, y) {
+    context.fillStyle = "green";
+    context.fillRect(position * ARROW_WIDTH + 10, y + 10, 80, 80);
+}
+
+function arrowPositions(beat) {
+    const result = [];
+    if (beat.includes("L")) {
+        result.push(0);
+    }
+    if (beat.includes("D")) {
+        result.push(1);
+    }
+    if (beat.includes("U")) {
+        result.push(2);
+    }
+    if (beat.includes("R")) {
+        result.push(3);
+    }
+    return result;
+}
